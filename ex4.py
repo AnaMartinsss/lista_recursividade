2f28a3c9150d2e051c86a4f9816a17727f05b1a5
AnaMartinsss <martinsanalaura108@gmail.com>
2025-02-11 22:47:25 -0300

    ..

---
+++ ex4.py
@@ -0,0 +1,10 @@
+'''
+Exercício 4: Encontrar o Índice do Maior Elemento
+Escreva uma função recursiva chamada indice_maior_elemento(lista) que retorna o
+índice do maior elemento em uma lista.
+Exemplo de Entrada:
+indice_maior_elemento([1, 5, 3, 9, 2])
+Saída Esperada:
+3 # O maior elemento é 9, que está no índice 3
+
+'''
